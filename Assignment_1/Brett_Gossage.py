ebf60cdd2447bb46ca291d35b480aa023b9e5bdb
bgossage <bgossage@invariant-corp.com>
2016-08-22 19:27:32 -0500

    Added user entry of the desired volume

--- Assignment_1/Brett_Gossage.py
+++ Assignment_1/Brett_Gossage.py
@@ -8,39 +8,65 @@
 # Assignment 1
 #
 
+import sys
+
+#
+# Ka is the dissociation constant
+# 
+# pKa = -log( Ka )
+#
+
+
 #
 # Define the list of available stock buffers...
 #
 solutions = { "Tris" : "8.072", 
               "Succinic acid" : "4.207",
-              "CABS" : "10.7"
+              "CABS" : "10.7",
+              "KH2PO4" : "7.2"
             }
 
 try:
    print "Available buffers: ", solutions
    
-# Get the desired buffer solution...
-   buffer = raw_input("Enter the buffer: " )
+   buffer = "";
    
-   print "Selected buffer: ", buffer
-   print "Selected pKa: ", solutions[buffer]
+   while( buffer not in solutions ):
    
-# Find the buffer pKa...
-   pKa = float( solutions[buffer] )
+   # Get the desired buffer solution...
+      buffer = raw_input("Enter a valid buffer: " )
+
+   else:
+   # Find the buffer pKa...
+      pKa = float( solutions[buffer] )
    
-   print "pKa = ", pKa
+   print
+   print "Selected Solution: ", buffer
+   print "Solution pKa: ", pKa
+
 
 # Get desired pH from the user...
    pH = float(raw_input("Enter the desired pH: " ))
-   
-   if( pH < 0.0 ): raise Exception( "pH must be positive" )
 
    if( abs(pH-pKa) > 1.0 ): raise Exception( "pH is not within +-1.0 of the pKa" );
 
    print "Desired pH: ", pH
 
+# Get desired volume from the user...
+   volume = float(raw_input("Enter the desired volume (mL): " ))
+   
+   print "Desired volume: ", volume, " (mL)"
+
+#
+# NOTE: Assume that the Henderson-Hasselbach equation applies.
+#           =>      pH = pKa + log( [A] / [HA] )
+#                   A .=. concentration of the conjugate base
+#                   HA .=. concentration of the weak acid
+#
 
 
+
+   
 
 except Exception as err:
    print "An error occured: ", err
