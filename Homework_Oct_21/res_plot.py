2c6a12307121451a4843837b088802d7660d0bd0
bgossage <bgossagen@gmail.com>
2016-11-03 20:35:43 -0500

    Sorted the value pairs.  Added screening based on resolution ranges.

--- Homework_Oct_21/res_plot.py
+++ Homework_Oct_21/res_plot.py
@@ -35,8 +35,10 @@ print "filename: ", filename
 #
 with open( filename, "r" ) as sca_file:
 
+# Read all the lines in the file...
    lines = sca_file.readlines()
-   
+
+# Extract the unit cell parameters...
    cell_params = lines[2].split()
    
    print "Cell Params: ", cell_params
@@ -49,22 +51,30 @@ with open( filename, "r" ) as sca_file:
    unitCell.alpha = float( cell_params[3] )
    unitCell.beta = float( cell_params[4] )
    unitCell.gamma = float( cell_params[5] )
-   
+
+# Defne a list to store the values that pass screening...
    value_pairs = list()
    
    Inorm = 0.0
    d = 0.0
    
-   Ndatums = len(lines)-3
+   Ndatums = 0
    
-   print "Datums = ", Ndatums
 
+   threshold = 5.0
+   
+   thresholds = [ 1.0, 2.0, 3.0, 5.0, 10.0, 15.0 ]
+   
+   ranges = [ [29.44,10.6196],
+              [4.38, 3.48],
+              [3.48, 3.03]
+            ]
+
+
+# Process each scatter data line...
    for i in range( 3, len(lines) ):
       
    # Read h  k  l  I  sigma...
-
-   #    print lines[i]
-      
       tokens = lines[i].split();
       
       if len(tokens) == 0 : break
@@ -81,42 +91,46 @@ with open( filename, "r" ) as sca_file:
 
    # Compute I/sigma...
       Inorm = I / sigma
-      
 
-      if Inorm > 3.0 :
+   # Screen based on desired resolution range...
+      res_low = ranges[0][1]; res_high = ranges[0][0]
+
+      #if d >= res_low and d <= res_high :
+      Ndatums += 1
+
+   # Screen based on noise-weighted intensity threshold.
+      if Inorm > threshold :
+         #print h, " ", k, " ", l, " | ", d, "   ", I, " ", sigma, " : ", Inorm
          value_pairs.append( [d,Inorm] )
 
    #end for
-
-   N = len(value_pairs);
    
+# Sort the values on d-spacing...
+   value_pairs.sort( key = lambda pair : pair[0] ) 
+
+   N = len(value_pairs)
+
    print "N = ", N
-   
-   percent = 100.0 * float(N)/float(Ndatums)
-   
 
-   
-   print "Percent above threshold: ", percent
-   
+   print "Datums = ", Ndatums
+
+   percent = 100.0 * float(N)/float(Ndatums)
+
+   print "Percent within Res. range: ", percent
+
    x_values = numpy.zeros(N)
    y_values = numpy.zeros(N)
-   
-   for i in range(0,N):
-       x_values[i] = value_pairs[i][0]
-       y_values[i] = value_pairs[i][1]
-       
-   print "Resolution range: [ ", min(x_values), " , ", max(x_values), " ] "
-	   
-   index_array = numpy.argsort( x_values )
 
-   px_values = x_values[index_array]
-   py_values = y_values[index_array]
-   
-   matplotlib.pyplot.plot( px_values, py_values )
+   for i in range(0,N):
+      x_values[i] = value_pairs[i][0]
+      y_values[i] = value_pairs[i][1]
+
+   print "Resolution range: [ ", min(x_values), " , ", max(x_values), " ] "
+
+   matplotlib.pyplot.plot( x_values, y_values )
    matplotlib.pyplot.show()
-   
-   
-  
+
+
 # end with sca_file
 
 # Plot the I/sigma vs resolution d...
